bae54001f699e4047a69c4773641b9eb50daec07
gabrielabitca <gabrielabitca2005@gmail.com>
2026-03-10 02:25:24 +0200

    create Graph class and addEdge method

---
+++ main.py
@@ -0,0 +1,14 @@
+import time
+import random
+from collections import defaultdict, deque
+import matplotlib.pyplot as plt
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
