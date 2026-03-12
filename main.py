a29d9f3f56bc61f8dc1b82196c48571dcbd7b188
gabrielabitca <gabrielabitca2005@gmail.com>
2026-03-12 03:40:15 +0200

    Add graph for DFS and BFS

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ class Graph:
         visited = set()
         self.DFSUtil(start, visited)
 
-    # BFS implementation
     def BFS(self, start):
         visited = set()
         queue = deque()
@@ -40,56 +39,64 @@ class Graph:
                     visited.add(neighbour)
                     queue.append(neighbour)
 
-    def generate_graph(n, edges_per_node=3):
 
-        g = Graph(n)
+def generate_graph(n, edges_per_node=3):
+    g = Graph(n)
 
-        for i in range(n):
-            for _ in range(edges_per_node):
-                v = random.randint(0, n - 1)
+    for i in range(n):
+        for _ in range(edges_per_node):
+            v = random.randint(0, n - 1)
 
-                if v != i:
-                    g.addEdge(i, v)
+            if v != i:
+                g.addEdge(i, v)
 
-        return g
+    return g
 
-    values = [10, 50, 100, 200, 300, 400, 500]
 
-    dfs_times = []
-    bfs_times = []
+values = [10, 50, 100, 200, 300, 400, 500]
 
-    print("Nodes\tDFS Time\tBFS Time")
+dfs_times = []
+bfs_times = []
 
-    for n in values:
-        g = generate_graph(n)
+print("Nodes\tDFS Time\tBFS Time")
 
-        # DFS timing
-        start = time.perf_counter()
-        g.DFS(0)
-        end = time.perf_counter()
-        dfs_time = end - start
+for n in values:
 
-        # BFS timing
-        start = time.perf_counter()
-        g.BFS(0)
-        end = time.perf_counter()
-        bfs_time = end - start
+    g = generate_graph(n)
 
-        dfs_times.append(dfs_time)
-        bfs_times.append(bfs_time)
+    # DFS timing
+    start = time.perf_counter()
+    g.DFS(0)
+    end = time.perf_counter()
+    dfs_time = end - start
 
-        print(f"{n}\t{dfs_time:.8f}\t{bfs_time:.8f}")
+    # BFS timing
+    start = time.perf_counter()
+    g.BFS(0)
+    end = time.perf_counter()
+    bfs_time = end - start
 
-    plt.figure(figsize=(8, 5))
+    dfs_times.append(dfs_time)
+    bfs_times.append(bfs_time)
 
-    plt.plot(values, dfs_times, marker='o', label="DFS")
-    plt.plot(values, bfs_times, marker='o', label="BFS")
+    print(f"{n}\t{dfs_time:.8f}\t{bfs_time:.8f}")
 
-    plt.xlabel("Number of Nodes")
-    plt.ylabel("Execution Time (seconds)")
-    plt.title("Empirical Analysis of DFS vs BFS")
 
-    plt.legend()
-    plt.grid()
+# DFS PERFORMANCE GRAPH
+plt.figure()
+plt.plot(values, dfs_times, marker='o')
+plt.xlabel("Number of Nodes")
+plt.ylabel("Execution Time (seconds)")
+plt.title("DFS Performance Analysis")
+plt.grid()
+plt.show()
 
-    plt.show()
+
+# BFS PERFORMANCE GRAPH
+plt.figure()
+plt.plot(values, bfs_times, marker='o')
+plt.xlabel("Number of Nodes")
+plt.ylabel("Execution Time (seconds)")
+plt.title("BFS Performance Analysis")
+plt.grid()
+plt.show()
