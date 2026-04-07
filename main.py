11aedb9b79ff7fdf89dc4a0f41f9cb04161bf457
gabrielabitca <gabrielabitca2005@gmail.com>
2026-04-07 15:28:12 +0300

    Implement dijkstra using min heap

---
+++ main.py
@@ -0,0 +1,27 @@
+import heapq
+
+
+def dijkstra(graph, start):
+    """
+    graph: adjacency list where graph[u] = [(v1, w1), (v2, w2), ...]
+    start: starting node
+    returns: shortest distance from start to all other nodes
+    """
+    n = len(graph)
+    distances = [float('inf')] * n
+    distances[start] = 0
+    visited = [False] * n
+    heap = [(0, start)]  # (distance, node)
+
+    while heap:
+        current_distance, u = heapq.heappop(heap)
+        if visited[u]:
+            continue
+        visited[u] = True
+
+        for v, weight in graph[u]:
+            if distances[u] + weight < distances[v]:
+                distances[v] = distances[u] + weight
+                heapq.heappush(heap, (distances[v], v))
+
+    return distances
