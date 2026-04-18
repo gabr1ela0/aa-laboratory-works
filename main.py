a3d8aa5ba623447f48c5ddf3c7bfb6e632c5cd16
gabrielabitca <gabrielabitca2005@gmail.com>
2026-04-19 00:27:00 +0300

    add UnionFind class

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+# Union-Find for Kruskal
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx, ry = self.find(x), self.find(y)
+        if rx == ry:
+            return False
+        if self.rank[rx] < self.rank[ry]:
+            rx, ry = ry, rx
+        self.parent[ry] = rx
+        if self.rank[rx] == self.rank[ry]:
+            self.rank[rx] += 1
+        return True
+
